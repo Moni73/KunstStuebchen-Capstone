65e5a7eb0cf9fa3c969ca0e2ef64ea965247a3c8
Moni73 <100587808+Moni73@users.noreply.github.com>
2022-05-05 21:16:42 +0200

    ExhibitionDates2

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./pages/about/About.jsx";
+import Experience from "./pages/experience/Experience.jsx";
 
 function App() {
   return (
     <>
       <About />
+      <Experience />
     </>
   );
 }
---
+++ client/src/pages/experience/Experience.jsx
@@ -0,0 +1,47 @@
+import "./experience.css";
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="d-flex justify-content-center my-5">
+        <h1>experience</h1>
+      </div>
+      <div className="container experience-wrapper">
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2019</h3>
+            <p>Gründung</p>
+          </div>
+        </div>
+
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2020</h3>
+            <p>Kunst-Wettbewerb</p>
+          </div>
+        </div>
+
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2021</h3>
+            <p>Entwicklung neuer Techniken</p>
+          </div>
+        </div>
+
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2022</h3>
+            <p>Ausstellung geplant</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
