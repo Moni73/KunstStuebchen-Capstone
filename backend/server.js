d7a8e8b7fcf35a31eacd78b5445faec71ecfd0cb
Moni73 <100587808+Moni73@users.noreply.github.com>
2022-06-07 11:25:09 +0200

    herokubackend

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,23 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const { json } = require("body-parser");
 const axios = require("axios");
-
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(json());
 
-const { parsed: config } = dotenv.config();
+//const { parsed: config } = dotenv.config();
 
-const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}`;
+const CLOUD_NAME = process.env.CLOUD_NAME;
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
+
+const BASE_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}`;
 
 const auth = {
-  username: config.API_KEY,
-  password: config.API_SECRET,
+  username: API_KEY,
+  password: API_SECRET,
 };
 
 app.get("/photos", async (req, res) => {
